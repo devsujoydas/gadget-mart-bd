feb229bbd6c7e1b7bf347412a8e16f3ee6b9ea19
unknown <sujoydaspc2023@gmail.com>
2025-01-04 23:14:32 +0600

    payment.jsx payment section done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = ({ setToggle }) => {
   const navStyle = "hover:text-emerald-600 px-2 py-1 rounded-lg  hover:font-semibold active:scale-95 transition-all";
   return (
-    <div className="shadow-2xl ">
+    <div className="shadow-lg ">
       <nav className="max-w-screen-2xl md:mx-auto mx-10 md:flex-row flex-col flex justify-between items-center py-6 ">
         <NavLink to={"/"}><img className="w-[250px]" src="/public/assets/navlogo.png" alt="nav-logo" /></NavLink>
         <div id="nav-menus" className="flex gap-14 font-medium">
--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -1,9 +1,12 @@
 
 const Payment = () => {
+    const formHandler = e => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            <div className="max-w-screen-2xl lg:mx-auto mx-10 grid md:grid-cols-2 grid-cols-1" >
-                <div className="grid gap-5">
+        <div className="my-10">
+            <div id="confirm" className="max-w-screen-2xl lg:mx-auto mx-10 grid md:grid-cols-2 grid-cols-1" >
+                <div className="grid gap-5 border">
                     <h1 className="text-2xl font-medium">Payment Method</h1>
                     <div className="bg-gray-200 w-fit p-2 flex gap-2 rounded-lg text-xl font-medium">
                         <button className="bg-white p-2 rounded-lg">Cash On Delivery</button>
@@ -17,16 +20,20 @@ const Payment = () => {
                         <input type="number" id="phone-number" name="phone-number" placeholder="Phone Number" className="border px-4 py-2 rounded-md" />
                         <label htmlFor="address">Full Address <span className="text-red-500">*</span></label>
                         <textarea type="text" id="address" name="address" placeholder="Address" className="border px-4 py-2 rounded-md" />
-                        <div>
-                            <input type="checkbox" />
-                            <h1>I have read andd agree to the <span>Terms and Conditions</span> and <span>Privacy Policy</span></h1>
+                        <div className="flex justify-between items-center mt-8">
+                            <div className="text-lg flex items-center gap-2">
+                                <input type="checkbox" />
+                                <h1>I have read andd agree to the <a href="#confirm" className="text-[#F27F20]">Terms and Conditions</a> and <a href="#confirm" className="text-[#F27F20]">Privacy Policy</a></h1>
+                            </div>
+                            <button onClick={formHandler} className="bg-[#F27F20] font-medium text-white px-4 py-2 rounded-md hover:bg-transparent active:scale-95 border hover:border-black hover:text-black transition-all">Confirm</button>
                         </div>
-                        <button>Confirm</button>
 
                     </form>
                 </div>
 
-                <div></div>
+                <div className="px-10">
+                <h1 className="text-2xl font-medium">Our Order</h1>
+                </div>
             </div>
         </div>
     )
